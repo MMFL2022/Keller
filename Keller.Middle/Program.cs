d35484edd1768d1caf230e8a252fd2bf3fc0127c
Mike Miller <mike.miller@mikeslab.co>
2022-12-22 17:30:49 -0500

    Moved middle character logic into a separate method per the requirements.

--- Keller.Middle/Program.cs
+++ Keller.Middle/Program.cs
@@ -17,28 +17,35 @@ namespace Keller.Middle
                 Console.WriteLine("Enter a string:");
                 var inputString = Console.ReadLine();
 
-                int inputStringLength = inputString.Length;
-                if (inputStringLength != 0)
+                var middle = GetMiddleCharacter(inputString);
+                if (middle != null)
+                    Console.WriteLine($"The middle character(s): {middle}");
+                else
+                    Console.WriteLine("Your input did not contain any characters.  Re-run the program to try again.");
+            }
+        }
+
+        private static string GetMiddleCharacter(string input)
+        {
+            int inputStringLength = input.Length;
+            if (inputStringLength != 0)
+            {
+                if (inputStringLength % 2 == 0)
                 {
-                    if (inputStringLength % 2 == 0)
-                    {
-                        // even number of characters
-                        var start = inputStringLength / 2;
-                        var middleCharacter = inputString.Substring(start - 1, 2);
-                        Console.WriteLine($"Middle characters are '{middleCharacter}'");
-                    }
-                    else
-                    {
-                        // odd number of characters
-                        var start = (inputStringLength - 1) / 2;
-                        var middleCharacter = inputString.Substring(start, 1);
-                        Console.WriteLine($"Middle character is '{middleCharacter}'");
-                    }
+                    // even number of characters
+                    var start = inputStringLength / 2;
+                    return input.Substring(start - 1, 2);
                 }
                 else
                 {
-                    Console.WriteLine("Your input did not contain any characters.  Re-run the program to try again.");
+                    // odd number of characters
+                    var start = (inputStringLength - 1) / 2;
+                    return input.Substring(start, 1);
                 }
+            }
+            else
+            {
+                return null;
             }
         }
     }
